09462d35675d04f9697e4d0c275a0925d26f3013
Evgenia Stepanova <evgeniast85es@yandex.ru>
2023-06-06 10:18:16 +0300

    fix localhost to db

--- app.js
+++ app.js
@@ -5,8 +5,9 @@ import userRouter from './routes/users.js';
 const { PORT = 3000 } = process.env;
 const app = express();
 
-mongoose.connect('mongodb://localhost:27017/mestodb');
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
+app.use(express.json());
 app.use('/users', userRouter);
 
 // app.use(express.static(path.join(__dirname, 'public')));
