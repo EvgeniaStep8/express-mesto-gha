85938f9b5a69ab7fde112322d537844cc71631f9
Evgenia Stepanova <evgeniast85es@yandex.ru>
2023-06-06 15:34:41 +0300

    feat: add like and dislike cards by /cards/:cardId/likes

--- controllers/cards.js
+++ controllers/cards.js
@@ -20,8 +20,30 @@ const deleteCard = (req, res) => {
     .catch((err) => res.status(500).send({ message: err.message }));
 };
 
+const likeCard = (req, res) => {
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $addToSet: { likes: req.user._id } },
+    { new: true },
+  )
+    .then((likes) => res.send(likes))
+    .catch((err) => res.status(500).send({ message: err.message }));
+};
+
+const dislikeCard = (req, res) => {
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $pull: { likes: req.user._id } },
+    { new: true },
+  )
+    .then((likes) => res.send(likes))
+    .catch((err) => res.status(500).send({ message: err.message }));
+};
+
 module.exports = {
   getCards,
   createCard,
   deleteCard,
+  likeCard,
+  dislikeCard,
 };
--- routes/card.js
+++ routes/card.js
@@ -1,8 +1,16 @@
 const cardRoutes = require('express').Router();
-const { getCards, createCard, deleteCard } = require('../controllers/cards');
+const {
+  getCards,
+  createCard,
+  deleteCard,
+  likeCard,
+  dislikeCard,
+} = require('../controllers/cards');
 
 cardRoutes.get('/', getCards);
 cardRoutes.post('/', createCard);
 cardRoutes.delete('/:id', deleteCard);
+cardRoutes.put('/:cardId/likes', likeCard);
+cardRoutes.delete('/:cardId/likes', dislikeCard);
 
 module.exports = cardRoutes;
