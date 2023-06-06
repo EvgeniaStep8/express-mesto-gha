1ec066c38f5f62fe952eb162808b320d3f6cb0f2
Evgenia Stepanova <evgeniast85es@yandex.ru>
2023-06-06 11:26:58 +0300

    fix jsonparser

--- app.js
+++ app.js
@@ -5,9 +5,12 @@ const userRouter = require('./routes/users');
 const { PORT = 3000 } = process.env;
 const app = express();
 
-mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
+  useNewUrlParser: true,
+});
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use('/users', userRouter);
 
 // app.use(express.static(path.join(__dirname, 'public')));
