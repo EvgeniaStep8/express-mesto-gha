3fb62a530c8ce5158de8bbebf57f1aee47402ed0
Evgenia Stepanova <evgeniast85es@gmail.com>
2023-06-22 12:17:38 +0300

    feat: add helmet and rate-limiter

--- app.js
+++ app.js
@@ -1,19 +1,28 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
-const cokieParser = require('cookie-parser');
+const cookieParser = require('cookie-parser');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
 require('dotenv').config();
 const router = require('./routes/index');
 const { createUser, login } = require('./controllers/users');
 const { validateCreateUser, validateLogin } = require('./middleware/validation');
-const auth = require('./middleware/auth');
 const errorHandler = require('./middleware/errorHandler');
 
 const { PORT = 3000 } = process.env;
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  minutesmax: 100,
+  standardHeaders: true,
+  legacyHeaders: false,
+});
 
 const app = express();
 app.use(express.json());
-app.use(cokieParser());
+app.use(cookieParser());
+app.use(helmet());
+app.use(limiter);
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
   useNewUrlParser: true,
@@ -22,9 +31,7 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
 app.post('/signup', validateCreateUser, createUser);
 app.post('/signin', validateLogin, login);
 
-app.use(auth);
 app.use(router);
-
 app.use(errors());
 app.use(errorHandler);
 
