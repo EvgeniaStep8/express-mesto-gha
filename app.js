b8bc1dc799f011ea2b61a935cba6334146631801
Evgenia Stepanova <evgeniast85es@yandex.ru>
2023-06-24 12:31:41 +0300

    feat: add winston and express-winston for handle logs

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const { createUser, login } = require('./controllers/users');
 const { validateCreateUser, validateLogin } = require('./middleware/validation');
 const auth = require('./middleware/auth');
 const errorHandler = require('./middleware/errorHandler');
+const { requestLogger, errorLogger } = require('./middleware/logger');
 
 const { PORT = 3000 } = process.env;
 const limiter = rateLimit({
@@ -29,12 +30,15 @@ mongoose.connect('mongodb://127.0.0.1:27017/mestodb', {
   useNewUrlParser: true,
 });
 
+app.use(requestLogger);
+
 app.post('/signup', validateCreateUser, createUser);
 app.post('/signin', validateLogin, login);
 
 app.use(auth);
 app.use(router);
 
+app.use(errorLogger);
 app.use(errors());
 app.use(errorHandler);
 
